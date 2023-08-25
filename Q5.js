f75c05b893eceb8b1031b96134d99d48500e7eb8
emmanueil1 <emmanueiljames@gmail.com>
2023-08-25 16:49:26 +0500

    Q5

---
+++ Q5.js
@@ -0,0 +1,16 @@
+"use strict";
+// //Q5 - Create a program that determines the category of a user-provided age.
+// //If the age is between 0 and 12, print "Child.
+// //" If it's between 13 and 19, print "Teenager." Otherwise, print "Adult."
+function determineAgeCategory(age) {
+    if (age >= 0 && age <= 12) {
+        console.log("Child");
+    }
+    else if (age >= 13 && age <= 19) {
+        console.log("Teenager");
+    }
+    else {
+        console.log("Adult");
+    }
+}
+console.log("Age Category is"), determineAgeCategory(13);
