38f38eddd00fbae2cd9e19a7cc2054090eb1999e
emmanueil1 <emmanueiljames@gmail.com>
2023-08-25 10:25:27 +0500

    Q2

---
+++ Q2.js
@@ -0,0 +1,7 @@
+"use strict";
+// write a program that calculate the percentage.
+function percentage(givenValue, totalValue) {
+    var percentageformula = (givenValue / totalValue) * 100;
+    console.log("Percentage is =", percentageformula);
+}
+percentage(640, 850);
---
+++ Q2.ts
@@ -0,0 +1,7 @@
+// write a program that calculate the percentage.
+function percentage(givenValue:number,totalValue:number)
+{
+    var percentageformula:number=(givenValue/totalValue)*100;
+    console.log("Percentage is =",percentageformula);
+}
+percentage(640,850)
